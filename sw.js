7510991613dfad6cd7316a10dd0f7a15c2fa014f
Ali Karbassi <ali@karbassi.com>
2019-08-01 12:41:39 -0500

    Moved files into folders

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ which is in turn a modified version of Jeremy Keith's service worker (https://ad
 with a few additional edits borrowed from Filament Group's. (https://www.filamentgroup.com/sw.js)
 */
 (function () {
-    const version = 'v2';
+    const version = 'v4';
     const cacheName = ':karbassi-2019:';
 
     const staticCacheName = version + cacheName + 'static';
